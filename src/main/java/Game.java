552d3dcc558942a842e4f14d40bdc6e893b87942
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2022-10-07 14:26:41 +0200

    Movement åt alla håll är inlagda i en switch case som funkar :D

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,17 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     //Player player = new Player();
     public static ArrayList<GameObject> objects = new ArrayList<GameObject>();
     Map map = new Map();
-
+    Scanner scan = new Scanner(System.in);
     public Game() {
 
-
-        map.mapBuilder(map.mapTemplate1);
-
-
-        map.PrintMap(objects);
+        while(true) {
+            map.mapBuilder(map.mapTemplate1);
+            map.PrintMap(objects);
+            String input = scan.nextLine();
+            switch (input) {
+                case "a":
+                    MoveLeft(map.player1);
+                    break;
+                case "d":
+                    MoveRight(map.player1);
+                    break;
+                case "w":
+                    MoveUp(map.player1);
+                    break;
+                case "s":
+                    MoveDown(map.player1);
+                    break;
+                default:
+                    System.out.println("Snälla skriv korrekt. Annars blir apan ledsen.");
+            }
+        }
 
 
     }
@@ -20,32 +37,32 @@ public class Game {
     // Move Right
     public boolean MoveLeft(Player gameObject) {
 
-        String tile = map.ReturnTile(gameObject.getY(), gameObject.getX() + 1);        // Get what kind of tile we're stepping on.
+        String tile = map.ReturnTile(gameObject.getY(), gameObject.getX() - 1);        // Get what kind of tile we're stepping on.
 
         if (tile.equals("")) {
             System.out.println("That's out of bounds!");
             return false;
-        } else if (tile.equals("w")) {
+        } else if (tile.equals("W")) {
             System.out.println("That's a wall!");
             return false;
         }
-        gameObject.setX(gameObject.getX() + 1); // Move the object's x one step right.
+        gameObject.setX(gameObject.getX() - 1); // Move the object's x one step right.
         return true;
     }
 
     //Move Down -Ner i Y led = plus 1
     public boolean MoveRight(Player gameObject) {
 
-        String tile = map.ReturnTile(gameObject.getY() + 1, gameObject.getX());         // Get what kind of tile we're stepping on.
+        String tile = map.ReturnTile(gameObject.getY() , gameObject.getX()+ 1);         // Get what kind of tile we're stepping on.
 
-        if (tile.equals("")) {
+        if (tile.equals("")){
             System.out.println("That's out of bounds!");
             return false;
-        } else if (tile.equals("w")) {
+        } else if (tile.equals("W")) {
             System.out.println("That's a wall!");
             return false;
         }
-        gameObject.setY(gameObject.getY() + 1); // Move the object's x one step down.
+        gameObject.setX(gameObject.getX() + 1); // Move the object's x one step down.
         return true;
     }
 
@@ -57,7 +74,7 @@ public class Game {
         if (tile.equals("")) {
             System.out.println("That's out of bounds!");
             return false;
-        } else if (tile.equals("w")) {
+        } else if (tile.equals("W")) {
             System.out.println("That's a wall!");
             return false;
         }
@@ -65,20 +82,20 @@ public class Game {
         return true;
     }
 
-    //Move Left
+    //Move Down
     public boolean MoveDown(Player gameObject) {
 
-        String tile = map.ReturnTile(gameObject.getY(), gameObject.getX() - 1);         // Get what kind of tile we're stepping on.
+        String tile = map.ReturnTile(gameObject.getY() + 1, gameObject.getX());         // Get what kind of tile we're stepping on.
 
         if (tile.equals("")) {
             System.out.println("That's out of bounds!");
             return false;
-        } else if (tile.equals("w")) {
+        } else if (tile.equals("W")) {
             System.out.println("That's a wall!");
             return false;
         }
 
-        gameObject.setX(gameObject.getX() - 1); // Move the object's x one step left.
+        gameObject.setY(gameObject.getY() + 1); // Move the object's x one step down.
         return true;
     }
 
